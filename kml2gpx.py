67ddd1d15a386a4cd200bcb129e4bb126a89366e
Konstantin Läufer <klaeufer@gmail.com>
2020-06-24 10:37:34 -0500

    cleaned up code, added logging
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- kml2gpx.py
+++ kml2gpx.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
+import logging
 
 from pykml import parser
 from pykml.parser import Schema
@@ -9,22 +10,27 @@ from pykml.helpers import set_max_decimal_places
 import gpxpy
 import gpxpy.gpx
 from lxml import etree
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 from tqdm import tqdm
+
+logging.basicConfig(level=logging.DEBUG)
 
 # parse kml
 
 root = parser.parse(sys.stdin).getroot()
-print('kml22gx validation:', Schema("kml22gx.xsd").validate(root), file=sys.stderr)
-print('ogckml22 validation:', Schema("ogckml22.xsd").validate(root), file=sys.stderr)
 set_max_decimal_places(root, max_decimals={'longitude': 6,'latitude': 6})
+
+logging.info('kml22gx validation: %r', Schema("kml22gx.xsd").validate(root))
+logging.info('ogckml22 validation: %r', Schema("ogckml22.xsd").validate(root))
 
 # https://developer.mozilla.org/en-US/docs/Web/XPath/Functions
 # https://devhints.io/xpath
 
-placemarks = root.xpath('/k:kml//k:Placemark', namespaces={'k': 'http://www.opengis.net/kml/2.2'})
-print(f'found {len(placemarks)} placemarks', file=sys.stderr)
+placemarks = root.xpath(
+    '//k:Placemark',
+    namespaces={'k': 'http://www.opengis.net/kml/2.2'})
+logging.info('found %s placemarks', len(placemarks))
 
 # TODO read these fields from the kml file itself!
 # TODO make the mapping to their display names configurable
@@ -46,32 +52,33 @@ fields = {
 
 # write gpx
 
-def find_text_for_attrib(sdata, name):
+def find_text_for_field(sdata, name):
     return ''.join(sdata.xpath(f'./*[@name="{name}"]/text()')).strip()
-    
+
+def find_plain_text_for_field(sdata, name):
+    return bs(find_text_for_field(sdata, name), features="lxml").get_text()
+
 gpx = gpxpy.gpx.GPX()
 count = 0
 for e in tqdm(placemarks):
     try:
         sdata = e.ExtendedData.SchemaData
         try:
-            pos = e.Point.coordinates.text.split(',')
-            lon = pos[0]
-            lat = pos[1]
+            lon, lat = e.Point.coordinates.text.split(',')
         except:
-            print('no Point found, using LON/LAT', file=sys.stderr)
-            lon = find_text_for_attrib(sdata, "LONGITUDE")
-            lat = find_text_for_attrib(sdata, "LATITUDE")
+            logging.info('no Point found, using LON/LAT')
+            lon = find_text_for_field(sdata, "LONGITUDE")
+            lat = find_text_for_field(sdata, "LATITUDE")
         if float(lon) > 0:
             # lat/lon probably in wrong order
             lat, lon = lon, lat
-            print(f'WARNING: switched lat/lon {lat}, {lon}', file=sys.stderr)
-        wpt = gpxpy.gpx.GPXWaypoint(longitude = lon, latitude = lat)
-        wpt.name = find_text_for_attrib(sdata, "RECAREANAM")
-        desc = BeautifulSoup(find_text_for_attrib(sdata, "RECAREADES"), features="lxml").get_text()
+            logging.warning(f'switched lat/lon {lat}, {lon}')
+        wpt = gpxpy.gpx.GPXWaypoint(latitude = lat, longitude = lon)
+        wpt.name = find_text_for_field(sdata, "RECAREANAM")
+        desc = find_plain_text_for_field(sdata, "RECAREADES")
         desc += '\n'
         for field in fields:
-            fdata = BeautifulSoup(find_text_for_attrib(sdata, field), features="lxml").get_text()
+            fdata = find_plain_text_for_field(sdata, field)
             if len(fdata) > 0:
                 desc += '\n'
                 desc += fields[field]
@@ -79,6 +86,6 @@ for e in tqdm(placemarks):
         wpt.description = desc
         gpx.waypoints.append(wpt)
     except:
-        print("problem with placemark", find_text_for_attrib(e.ExtendedData.SchemaData, "RECAREANAM"), file=sys.stderr)
+        logging.error("problem with placemark", find_text_for_field(e.ExtendedData.SchemaData, "RECAREANAM"))
 
 print(gpx.to_xml())
